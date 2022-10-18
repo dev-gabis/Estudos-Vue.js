1e160964795c3f8575582a2d935491bd7f15f582
Gabi reis <reisgabriela293@gmail.com>
2022-10-18 14:23:34 -0300

    ♻️  fist commit

---
+++ 4_componentes/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <primeirocomponente />
+</template>
+
+<script>
+    import primeirocomponente from './components/PrimeiroComponente.vue'
+
+    export default {
+        name: 'App',
+        components: {
+            primeirocomponente
+        }
+    }
+</script>
---
+++ 4_componentes/src/components/PrimeiroComponente.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Olá vue!</h1>
+</template>
+
+<script>
+export default {
+    name:  'primeirocomponente'
+
+}
+</script>
